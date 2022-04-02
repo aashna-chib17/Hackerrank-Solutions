fa720b5f5b433bf9bb4ddf174f88b80d0497c7e1
Aashna Chib <aashnachib17@gmail.com>
2022-04-02 22:04:48 +0530

    Update Shifting Strings.py

--- Shifting Strings.py
+++ Shifting Strings.py
@@ -1,3 +1,5 @@
+# Solution 1
+
 s = input()
 s = list(s)
 leftShifts = int(input())
@@ -9,3 +11,13 @@ for _ in range(rightShifts):
     a = s.pop()
     s.insert(0,a)
 print(''.join(s))
+
+
+# Solution 2
+
+s= input()
+leftShifts = int(input())
+rightShifts = int(input())
+i = (leftShifts - rightShifts) % len(s)
+print(s[i:]+s[:i])
+
